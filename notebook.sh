c7fbf93ed6a526fb32b20e4562e61a3fb61d250b
con <165208296+cons-code@users.noreply.github.com>
2025-01-19 19:52:40 -0500

    Update notebook.sh

--- notebook.sh
+++ notebook.sh
@@ -4,8 +4,8 @@
 docker pull c000llc/docker:ML
 
 #Enter: port, mount, aforementioned
-docker run --detatch -rrm -p 5253:8888 \ 
-	-- name ML \
-	-- mount type=bind, source=/Users/c.000llc/Development,target=/home/jovyan/ work \
+docker run --detach --rm -p 5253:8888 \
+	--name ML \
+	--mount type=bind,source=/Users/amy/harpoon,target=/home/jovyan/work \
 	-e JUPYTER_TOKEN='#Whiteeagles5253jtdc###' \
 	c000llc/docker:ML
